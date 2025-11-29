6239af9ebc25d5c0ab6c52066798fc972ddd6f5f
Viqtor <viqto0r@gmail.com>
2025-11-29 18:33:59 +0300

    feat: add new functions

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const divide = (a, b) => a / b
 
 const subtract = (a, b) => a - b
 
-module.exports = { sum, multiply, divide, subtract }
+const pow = (a, b) => a ** b
+
+const root = (a, b) => a ** (1 / b)
+
+const log = (a, b) => Math.log(a) / Math.log(b)
+
+module.exports = { sum, multiply, divide, subtract, pow, root, log }
